79fd07472513ba625985b939c31c652fa95b350a
J-hoplin1 <jhoplin7259@gmail.com>
2022-07-29 01:18:23 +0900

    feat(platfoms) : Weather Hook for discord and slack

---
+++ Hooks/data.js
@@ -0,0 +1,197 @@
+const axios = require('axios')
+const config = require('./config.json')
+
+const getAQIValue = async (aqi) => {
+    switch(aqi){
+        case 1:
+            return "매우좋음"
+        case 2:
+            return "좋음"
+        case 3:
+            return "보통"
+        case 4:
+            return "나쁨"
+        case 5:
+            return "매우나쁨"
+        case 6:
+            return "Unknow AQI Value"
+    }
+}
+
+const getAirPollutionInfo = async (cityName,lat,lon) => {
+    try{
+        const reqParam = {
+            lat: lat,
+            lon: lon,
+            appid: config.api_key,
+        }
+        const res = await axios.get(`${config.api_endpoint_air_pollution}?${new URLSearchParams(reqParam)}`)
+        const {
+
+            main: { aqi },
+            components : {
+                co,
+                pm2_5,
+                pm10
+            },
+            dt
+        } = res.data.list[0]
+        return {
+            co : co,
+            pm2_5 : pm2_5,
+            pm10 : pm10,
+            aqi : await getAQIValue(aqi),
+            measuredTime_airpollution : dt
+        }
+    }catch(err){
+        return false
+    }
+}
+
+const getWeatherInfo = async (cityName="seoul") => {
+    // Get longtitude, latitude, locale_name(as korean) infos
+    const locationRes = await getLongLatInfo(cityName);
+    try{
+        locationRes.code
+        ? (() => {throw new Error()})()
+        :true
+        const { locale_name,lat,lon } = locationRes.data
+        const reqParam = {
+            lat: lat,
+            lon: lon,
+            appid: config.api_key,
+            units: 'metric'
+        }// units : this option make temperature into celcius(Default : kalvin)
+        // Make Request as get
+        const res = await axios.get(`${config.api_endpoint}?${new URLSearchParams(reqParam)}`)
+        // Destruct response
+        const { 
+            weather:[
+            { 
+                main:weather,
+                description:weatherdescription,
+                icon:weather_icon
+            }
+            ],
+            main: {
+                temp:temperature,
+                feels_like:temperature_humanfeel,
+                temp_min,
+                temp_max,
+                pressure,
+                humidity
+            },
+            wind: {
+                speed:windspeed
+            },
+            clouds: {
+                all:cloudpercentage
+            },
+            dt:measuredTime,
+            sys: {
+                sunrise,
+                sunset
+            }
+        } = res.data
+        // Basic data capsule
+        // measuredTime : Unix Time -> Need to multiply 1000 for converting to JS Date obj
+        const airStatus = await getAirPollutionInfo(cityName,lat,lon);
+        const returnValue = {
+            code: 0,
+            data: {
+                locale_name: locale_name,
+                weather: weather,
+                weather_icon: `http://openweathermap.org/img/wn/${weather_icon.includes('n') ? weather_icon.replace('n','d') : weather_icon}@2x.png`,
+                description: weatherdescription,
+                temperature: temperature,
+                temperature_humanfeel: temperature_humanfeel,
+                temp_min: temp_min,
+                temp_max: temp_max,
+                pressure: pressure,
+                humidity: humidity,
+                windspeed: windspeed,
+                cloudpercentage:cloudpercentage,
+                sunrise: sunrise,
+                sunset: sunset,
+                measuredTime: measuredTime,
+                pm2_5 : airStatus.pm2_5,
+                pm10 : airStatus.pm10,
+                co : airStatus.co,
+                aqi : airStatus.aqi,
+                measuredTime_airpollution: airStatus.measuredTime_airpollution
+            }
+        }
+
+        // If weather rain
+        weather === "Rain"
+        ?(() => {
+            const {
+                rain: {
+                    '1h':rainPerHour
+                }
+            } = res.data
+            returnValue.data.rain_per_hour = rainPerHour
+        })()
+        :true
+        // If weather snow
+        weather === "Snow"
+        ?(() => {
+            const {
+                snow: {
+                    '1h':snowPerHour
+                }
+            } = res.data
+            returnValue.data.snow_per_hour = snowPerHour
+        })()
+        :true
+        return [cityName,returnValue]
+    }catch(err){
+        console.error(err)
+        // If fail
+        return [cityName,{
+            code: 1
+        }]
+    }
+}
+
+
+
+const getLongLatInfo = async (cityName) => {
+    // Make city name to lower case
+    cityName = cityName.toLowerCase();
+    // Geocoding latitude longtitude api parameter
+    const geoParam = {
+        q: cityName,
+        limit: 5,
+        appid: config.api_key
+    }  
+    try{
+        // Request API
+        const res = await axios.get(`${config.geo_coding_api_endpoint}?${new URLSearchParams(geoParam)}`)
+        // Destruct response json
+        const [{ local_names:{ ko }, lat, lon }] = res.data;
+        // If success
+        return {
+            code: 0,
+            data: {
+                locale_name: ko,
+                lat: lat,
+                lon: lon
+            }
+        }
+    }catch(err){
+        console.error(err)
+        // If fail
+        return {
+            code: 1
+        }
+    }
+}
+
+const getAllSupportedRegion = async() => {
+    const regionCalls = await Promise.all(config.supported_region.map(x => getWeatherInfo(x)))
+    return regionCalls
+}
+
+
+module.exports = { getAllSupportedRegion }
---
+++ platforms/slack/slack_hook_weather.js
@@ -0,0 +1,87 @@
+const schedule = require('node-schedule')
+const axios = require('axios')
+const config = require('./config.json')
+const common = require('../common.json')
+const { getAllSupportedRegion } = require('../../Hooks/data')
+
+const textBox = async (cityName,capsule) => {
+    let payload;
+    try{
+        const { code, data } = capsule
+        // If code 1 -> throw error
+        code ? (() => {throw new Error()})() : true
+        // Convert Unix time to normal millisecond and convert to Date Object
+        const measureTimeToDate = new Date(data.measuredTime * 1000)
+        const sunriseTimeToDate = new Date(data.sunrise * 1000)
+        const sunsetTimeToDate = new Date(data.sunset * 1000)
+        // Build Text
+        const integerStringFilter = (number) => {
+            return number < 10 ?  "0" + number : number.toString()
+        }
+
+        const textbox = `*[  ${data.locale_name}시 현재날씨  |   측정시간 : ${measureTimeToDate.getFullYear()}년 ${integerStringFilter(measureTimeToDate.getMonth() + 1)}월 ${integerStringFilter(measureTimeToDate.getDate())}일 ${integerStringFilter(measureTimeToDate.getHours())} : ${integerStringFilter(measureTimeToDate.getMinutes())} : ${integerStringFilter(measureTimeToDate.getSeconds())}  ]*\n
+    - *날씨* : ${data.weather}(${data.description})\n
+    - *온도* : ${data.temperature}℃ (체감온도 : ${data.temperature_humanfeel}℃)\n
+    - *최고/최저 기온* : ${data.temp_max}℃ / ${data.temp_min}℃\n
+    - *기압* : ${data.pressure} hPa\n
+    - *습도* : ${data.humidity} %\n
+    - *풍속* : ${data.windspeed} meter/sec\n
+    - *구름량* : ${data.cloudpercentage} %\n
+    - *일출/일몰 시간* : ${integerStringFilter(sunriseTimeToDate.getHours())}시 ${integerStringFilter(sunriseTimeToDate.getMinutes())}분 ${integerStringFilter(sunriseTimeToDate.getSeconds())}초 / ${integerStringFilter(sunsetTimeToDate.getHours())}시 ${integerStringFilter(sunsetTimeToDate.getMinutes())}분 ${integerStringFilter(sunsetTimeToDate.getSeconds())}초\n
+    - *대기질 상태* : ${data.aqi}\n
+    - *일산화탄소 농도* : ${data.co} μg/m3\n
+    - *미세먼지 / 초미세먼지 농도* : ${data.pm10} μg/m3 / ${data.pm2_5} μg/m3`
+        // Return city name and payload
+        return [cityName,{
+            "blocks": [
+                {
+                    "type": "section",
+                    "text": {
+                        "type" : "mrkdwn",
+                        "text" : textbox
+                    }
+                }
+            ]
+        }]
+    }catch(err){
+        return [cityName,{
+            "blocks": [
+                {
+                    "type": "section",
+                    "text": {
+                        "type" : "mrkdwn",
+                        "text" : common.fail_to_make_data
+                    }
+                }
+            ]
+        }] 
+    }
+}
+
+const preprocessTextBox = async() => {
+    try{
+        const regionDatas = await getAllSupportedRegion()
+        const regionMapper = new Map(await Promise.all(regionDatas.map(x => {
+            const [ cityName, returnValue ] = x
+            return textBox(cityName.toLowerCase(),returnValue)
+        })))
+        await Promise.all(Object.entries(config.weather_hooks_endpoints).map(x => {
+            const [ endpoint, region ] = x
+            return axios.post(endpoint,regionMapper.get(region.toLowerCase()))
+        }))
+    }catch(err){
+        console.error(err)
+    }
+}
+
+const scheduler = async () => {
+    try{
+        const scheduleObject = schedule.scheduleJob(config.scheduler_expression, async () => {
+            await preprocessTextBox()
+        })
+    }catch(err){
+        console.error(err)
+    }
+}
+
+scheduler()
